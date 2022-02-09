e3bf8b2f99a9806554819bb810f3c046fc44679c
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-09 14:33:32 +0100

    cart model final

--- laravel-ikt/app/Models/Cart.php
+++ laravel-ikt/app/Models/Cart.php
@@ -8,8 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {   
     protected $table = "carts";
-    
+
     public function user(){
         return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function cart_product_connection(){
+        return $this->hasMany(CartProductConnection::class, 'cartId');
     }
 }
