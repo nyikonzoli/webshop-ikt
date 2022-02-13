4b24a44f3a5853aedaad8969b62734eddddc50a0
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-13 11:27:36 +0100

    user seeder

--- laravel-ikt/database/seeders/UserSeeder.php
+++ laravel-ikt/database/seeders/UserSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +17,45 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Lewin Gedaliah',
+            'email' => 'lg@example.com',
+            'password' => Hash::make('password'),
+            'address' => Str::random(30),
+            'birthdate' => Carbon::parse('2000-01-01'),
+        ]);
+        DB::table('users')->insert([
+            'id' => 2,
+            'name' => 'Eemeli Nikolina',
+            'email' => 'en@example.com',
+            'password' => Hash::make('password'),
+            'address' => Str::random(30),
+            'birthdate' => Carbon::parse('2000-01-01'),
+        ]);
+        DB::table('users')->insert([
+            'id' => 3,
+            'name' => 'Adonijah Hormazd',
+            'email' => 'ah@example.com',
+            'password' => Hash::make('password'),
+            'address' => Str::random(30),
+            'birthdate' => Carbon::parse('2000-01-01'),
+        ]);
+        DB::table('users')->insert([
+            'id' => 4,
+            'name' => 'Basia Tamar',
+            'email' => 'bt@example.com',
+            'password' => Hash::make('password'),
+            'address' => Str::random(30),
+            'birthdate' => Carbon::parse('2000-01-01'),
+        ]);
+        DB::table('users')->insert([
+            'id' => 5,
+            'name' => 'Hinrik Vissarion',
+            'email' => 'hv@example.com',
+            'password' => Hash::make('password'),
+            'address' => Str::random(30),
+            'birthdate' => Carbon::parse('2000-01-01'),
+        ]);
     }
 }
