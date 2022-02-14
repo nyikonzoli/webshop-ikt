c65e05ca1c8cb6ba6fc1a44360d31813a01c1b15
BabikValter <babik.valter@njit.hu>
2022-02-14 13:41:13 +0100

    Product controller

---
+++ laravel-ikt/app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        return Product::all();
+    }
+
+    public function show($id) {
+        return Product::findOrFail($id)->get();
+    }
+
+    public function store(StoreProductRequest $request) {
+        $data = $request->validated();
+        return Product::create($data);
+    }
+
+    public function update(StoreProductRequest $request, $id) {
+        $data = $request->validated();
+        $product = Product::findOrFail($id);
+        $product->update($data);
+        return $product;
+    }
+
+    public function destroy($id) {
+        Product::destroy($id);
+    }
+}
