73754ffeb4d9ebc66d9a872935bef8807480d10c
BabikValter <babik.valter@njit.hu>
2022-02-18 16:13:39 +0100

    Cart Controller, Routes

---
+++ laravel-ikt/app/Http/Controllers/CartController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Http\Requests\CartStoreRequest;
+
+class CartController extends Controller
+{
+    public function index() {
+        return Cart::all();
+    }
+
+    public function show($id) {
+        return Cart::findOrFail($id)->get();
+    }
+
+    public function store(CartStoreRequest $request) {
+        $data = $request->validated();
+        return Cart::create($data);
+    }
+
+    public function update(CartStoreRequest $request, $id) {
+        $data = $request->validated();
+        $product = Cart::findOrFail($id);
+        $product->update($data);
+        return $product;
+    }
+
+    public function destroy($id) {
+        Cart::destroy($id);
+    }
+}
--- laravel-ikt/routes/api.php
+++ laravel-ikt/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,13 @@ Route::get('products/{id}', [ProductController::class, 'show'])->name('products.
 Route::post('products', [ProductController::class, 'store'])->name('products.store');
 Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
 Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+//Cart routes
+Route::get('carts', [CartController::class, 'index'])->name('carts.index');
+Route::get('carts/{id}', [CartController::class, 'show'])->name('carts.show');
+Route::post('carts', [CartController::class, 'store'])->name('carts.store');
+Route::put('carts/{id}', [CartController::class, 'update'])->name('carts.update');
+Route::delete('carts/{id}', [CartController::class, 'destroy'])->name('carts.destroy');
 
 //User routes
 Route::get('users', [UserController::class, "index"])->name("users.index");
