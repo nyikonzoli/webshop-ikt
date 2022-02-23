00335ac242af6779a3dc74a9e9f104287f1a7957
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-23 14:11:33 +0100

    image seeder

--- laravel-ikt/database/seeders/DatabaseSeeder.php
+++ laravel-ikt/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CartSeeder::class,
             CartProductConnectionSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
