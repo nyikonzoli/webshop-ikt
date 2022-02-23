c64cd692cdce54659c69a92bf06dde9620cff9d3
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-23 07:47:14 +0100

    route names

--- laravel-ikt/routes/web.php
+++ laravel-ikt/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/');
+
+Route::get('product/{id}');
+
+Route::get('profile');
+
+Route::get('cart');
