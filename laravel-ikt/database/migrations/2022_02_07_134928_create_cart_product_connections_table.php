dc9b535c9cd20559a0feb29734d91669b00fec8b
nyikonzoli <nyikon.zoli@gmail.com>
2022-02-24 10:51:47 +0100

    fix

---
+++ laravel-ikt/database/migrations/2022_02_07_134928_create_cart_product_connections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartProductConnectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product_connections', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->foreignId('cartId');
+            $table->foreignId('productId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_pruduct_connections');
+    }
+}
