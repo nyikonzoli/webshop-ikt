bcadc52b85731ba74e4a618847f1a10334e5aa0c
nyikonzoli <nyikon.zoli@gmail.com>
2022-02-24 11:48:01 +0100

    product start

--- laravel-ikt/resources/views/product.blade.php
+++ laravel-ikt/resources/views/product.blade.php
@@ -2,6 +2,20 @@
 
 @section('title', 'Product')
 
-@section('content')
+@section('header')
+    <script src="{{asset('js/product.js')}}"></script>
+@endsection
 
+@section('content')
+    <div id="everyting">
+        <div id="image">
+
+        </div>
+        <div id="data">
+            <h1></h1>
+            <h3></h3>
+            <p></p>
+            <button></button>
+        </div>
+    </div>
 @endsection
