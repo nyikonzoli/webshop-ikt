f5e6833f6a584d80e1365a77d8423d2f5d9ecf6a
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-03-02 08:53:39 +0100

    image controller

---
+++ laravel-ikt/app/Http/Controllers/ApiControllers/ImageController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreImageRequest;
+use App\Models\Image;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Image::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreImageRequest $request)
+    {
+        $data = $request->validated();
+        return Image::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Image::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreImageRequest $request, $id)
+    {
+        $data = $request->validated();
+        $image = Image::findOrFail($id);
+        $image->update($data);
+        return $image;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Image::destroy($id);
+    }
+}
