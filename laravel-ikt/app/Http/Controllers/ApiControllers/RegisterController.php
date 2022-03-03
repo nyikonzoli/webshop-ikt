b1bfbb63b01c3790ee4b099daddb81811cdbf4e0
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-03-03 19:18:54 +0100

    registration2

---
+++ laravel-ikt/app/Http/Controllers/ApiControllers/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterStoreRequest;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function store(RegisterStoreRequest $request)
+    {
+        $validated = $request->validated();
+        $validated["password"] = Hash::make($validated["password"]);
+        User::create($validated);
+        return back();
+    }
+}
