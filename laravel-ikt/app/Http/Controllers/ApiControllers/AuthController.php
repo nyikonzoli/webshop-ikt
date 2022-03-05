f07a0e516fbf67a18beab9be2d2c606865ee033a
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-03-05 15:48:36 +0100

    auth controller and loginrequest

---
+++ laravel-ikt/app/Http/Controllers/ApiControllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Request\LoginRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+    }
+
+    public function authentication()
+    {
+
+    }
+}
